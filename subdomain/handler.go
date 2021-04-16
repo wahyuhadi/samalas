c0f7fb4de836ccb7fe2c9b995c890027cebecefa
wahyuhadi <hrahmatwahyu@gmail.com>
2021-04-16 23:47:33 +0700

    Update: validate if is api key is avail in env

--- subdomain/handler.go
+++ subdomain/handler.go
@@ -1,14 +1,30 @@
 package subdomain
 
+import (
+	"os"
+)
+
 // - handler subdomain
 func HandlerSubdomain(domain string) []SubDomain {
+	// -- validate for SECURITYTRAILS_API_KEY
+	controller := ControllerScapper{}
+	controller.Securitytrails = true // -- default value is true
+	key := os.Getenv("SECURITYTRAILS_API_KEY")
+	if key == "" {
+		// -- change default value for Securitytrails
+		controller.Securitytrails = false
+	}
+
 	subDomainList := []SubDomain{}
 
 	hackerTarget := parseHackerTarget(domain)
 	subDomainList = append(subDomainList, SubDomain{Domains: hackerTarget, Source: "HackerTarget"})
 
-	securitytrails := ParseSecuritytrails(domain)
-	subDomainList = append(subDomainList, SubDomain{Domains: securitytrails, Source: "Securitytrails"})
+	// -- if is api key true for Securitytrails
+	if controller.Securitytrails {
+		securitytrails := ParseSecuritytrails(domain)
+		subDomainList = append(subDomainList, SubDomain{Domains: securitytrails, Source: "Securitytrails"})
+	}
 
 	return subDomainList
 }
